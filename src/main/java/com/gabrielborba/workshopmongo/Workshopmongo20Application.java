828001ca39f68e762b920b80731f87a925a8fe45
GabrielBorba2019 <gabrielborba.pm@Gmail.com>
2021-03-15 15:58:47 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/gabrielborba/workshopmongo/Workshopmongo20Application.java
@@ -0,0 +1,13 @@
+package com.gabrielborba.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongo20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongo20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielborba/workshopmongo/Workshopmongo20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielborba.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongo20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
